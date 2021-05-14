6cdaa211ff34e6db16f4288e829ec599ab745a37
umilic16 <77860394+umilic16@users.noreply.github.com>
2021-05-14 15:18:31 +0200

    reset

--- Projekat/moleculer.config.js
+++
@@ -1,8 +0,0 @@
-"use strict"; 
-const os = require("os");
-
-module.exports = {
-    nodeID: (process.env.NODEID ? process.env.NODEID + "-" : "") + os.hostname().toLowerCase(),
-    // metrics: true
-    // cacher: true
-};
